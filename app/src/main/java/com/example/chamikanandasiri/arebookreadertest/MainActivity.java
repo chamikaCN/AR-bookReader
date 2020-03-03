927597525193c52f526c19762be8268d237839b4
chamikaCN <chamikanandasiri97@gmail.com>
2020-03-03 19:18:13 +0530

    added new buttons

--- app/src/main/java/com/example/chamikanandasiri/arebookreadertest/MainActivity.java
+++ app/src/main/java/com/example/chamikanandasiri/arebookreadertest/MainActivity.java
@@ -20,25 +20,31 @@ import com.google.ar.sceneform.ux.TransformableNode;
 public class MainActivity extends AppCompatActivity {
 
     ArFragment arFragment;
-    Button nextButton;
+    Button textRecognizerButton, imageOverlayButton, corousalButton, hostButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nextButton = findViewById(R.id.button);
-        nextButton.setOnClickListener(v -> changeActivity());
+        textRecognizerButton = findViewById(R.id.textRecognizerButton);
+        imageOverlayButton = findViewById(R.id.overlayButton);
+        corousalButton = findViewById(R.id.corousalButton);
+        hostButton = findViewById(R.id.anchorHostButton);
+        textRecognizerButton.setOnClickListener(v -> changeActivity(1));
+        imageOverlayButton.setOnClickListener(v -> changeActivity(2));
+        corousalButton.setOnClickListener(v -> changeActivity(3));
+        hostButton.setOnClickListener(v -> changeActivity(4));
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
 
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
             Anchor anchor = hitResult.createAnchor();
 
             ModelRenderable.builder().setSource(this, Uri.parse("box.sfb")).build()
-                    .thenAccept(modelRenderable -> addModelToScene(anchor,modelRenderable))
+                    .thenAccept(modelRenderable -> addModelToScene(anchor, modelRenderable))
                     .exceptionally(throwable -> {
                         AlertDialog.Builder builder = new AlertDialog.Builder(this);
                         builder.setMessage(throwable.getMessage()).show();
-                        return  null;
+                        return null;
                     });
         });
     }
@@ -53,12 +59,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeTheModel(View view) {
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.textRecognizerButton);
         button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
     }
 
-    public void changeActivity(){
-        Intent intent = new Intent(this, TextDetectionActivity.class);
+    public void changeActivity(int activityNumber) {
+        Intent intent;
+        if (activityNumber == 1) {
+            intent = new Intent(this, TextDetectionActivity.class);
+        } else if (activityNumber == 3) {
+            intent = new Intent(this, CorousalActivity.class);
+        }else if (activityNumber == 4) {
+            intent = new Intent(this, AnchorHostActivity.class);
+        }else {
+            intent = new Intent(this, ImageOverlayActivity.class);
+        }
         startActivity(intent);
     }
 }
